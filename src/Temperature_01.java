521e3c4e02a7cef84c33321889a16a353b0ccfb2
MsSusai <liuhaorantony@163.com>
2020-09-03 22:33:02 +0800

    commit

---
+++ src/Temperature_01.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+//温度转换（5分）
+public class Temperature_01 {
+    public static void main(String[] args){
+        int f;
+        double c;
+        Scanner in = new Scanner(System.in);
+        f = in.nextInt();
+        c = (f-32)/(9.0/5.0);
+        System.out.println((int)c);
+    }
+}
